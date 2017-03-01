6435cef7dbc1bdb0b5e52bb042a0fdb2fb138606
Mauricio A. Ashimine <maashimine@gmail.com>
2017-03-01 01:46:00 -0300

    first_commit

---
+++ roles/letsencrypt/files/renewCerts.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This script renews all the Let's Encrypt certificates with a validity < 30 days
+
+if ! letsencrypt renew > /var/log/letsencrypt/renew.log 2>&1 ; then
+   echo Automated renewal failed:
+   cat /var/log/letsencrypt/renew.log
+   exit 1
+fi
+
+/usr/sbin/nginx -t && /usr/sbin/nginx -s reload
