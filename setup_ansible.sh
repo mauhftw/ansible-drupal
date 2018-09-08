aa3a4aab30db2f7061411d471bc5726f516cfc3d
Mark Mangoba <mmangoba@gmail.com>
2018-09-08 11:53:28 -0700

    adding prerequisites for ansbile vault

--- setup_ansible.sh
+++ setup_ansible.sh
@@ -6,5 +6,7 @@
   apt-get update
   apt-get install -y git python-dev python-pip python-openssl
   pip install --upgrade pip
+  sudo ln -sf $( type -P pip ) /usr/bin/pip
   pip install -U setuptools
   pip install ansible
+  pip install pycrypto
